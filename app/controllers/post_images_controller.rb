9c21eb8905410084242cdaffbe17e42de0dcf1ed
EC2 Default User <152859182+46nuko@users.noreply.github.com>
2024-02-04 01:51:30 +0000

    ２回目更新

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -14,6 +14,12 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+  end
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/post_images'
   end
 
   private
